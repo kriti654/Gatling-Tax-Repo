fc58002db128bc0c9511beab50500293976b95dc
kriti654 <80137830+kriti654@users.noreply.github.com>
2021-08-20 16:57:08 +0530

    Update AddCustomerSimulation.scala

--- src/test/scala/simulations/AddCustomerSimulation.scala
+++ src/test/scala/simulations/AddCustomerSimulation.scala
@@ -17,7 +17,11 @@ class AddCustomerSimulation extends Simulation {
       .post("/rvy/api/um/v1/tax")
       .body(RawFileBody(filePath = "./src/test/resources/bodies/addCustomer.json")).asJson
       .header(name="content-type",value = "application/json")
-      .check(status is 200)
+      .check(status is 200))
+      .exec(http("Get Tax By Id")
+       .get("/rvy/api/um/v1/tax/288")
+      .header(name="content-type",value = "application/json")
+      .check(status is 200)    
     )
 
 
