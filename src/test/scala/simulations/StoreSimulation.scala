740e9482900fe9c68fdfdf88d923a466b132072d
kriti654 <80137830+kriti654@users.noreply.github.com>
2021-08-21 22:47:18 +0530

    Update StoreSimulation.scala

--- src/test/scala/simulations/StoreSimulation.scala
+++ src/test/scala/simulations/StoreSimulation.scala
@@ -4,7 +4,7 @@ import io.gatling.core.scenario.Simulation
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class StoreTests extends Simulation{
+class StoreTestSimulation extends Simulation{
 
   //http conf
   val httpConf = http.baseUrl("http://localhost:8082")
